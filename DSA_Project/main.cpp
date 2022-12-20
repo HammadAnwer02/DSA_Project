461aa54976f5c4fcaff2be898bb83dfe9c69941f
Hammad Anwer <81057804+HammadAnwer02@users.noreply.github.com>
2022-12-20 22:35:03 +0500

    Matrix Visualization in Qt

--- DSA_Project/main.cpp
+++ DSA_Project/main.cpp
@@ -1,4 +1,4 @@
-#include "matrix.h"
+//#include "matrix.h"
 
 #include <QtCore/qglobal.h>
 #if QT_VERSION >= 0x050000
@@ -8,22 +8,49 @@
 #endif
 #include <QLocale>
 #include <QTranslator>
+#include<QLabel>
+#include<QImage>
 
 int main(int argc, char *argv[])
 {
+    int rows = 6, cols = 8;
     QApplication a(argc, argv);
+    QVector<QVector<int>> matrix = {{1, 2, 3, 255, 255, 6, 7, 8}, {1, 2, 3,255, 255, 6, 7, 8}, {1, 2, 3,255, 255, 6, 7, 8}, {1, 2, 3, 255, 255, 6, 7, 8}, {1, 2, 3, 255, 255, 6, 7, 8}, {1, 2, 3, 255, 255, 6, 7, 8}};
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "DSA_Project_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
+
+    QImage image(cols, rows, QImage::Format_RGB888);
+
+    for (int y = 0; y < rows; y++) {
+        for (int x = 0; x < cols; x++) {
+            matrix[y][x] = qRgb(0, 128, 0);
+            image.setPixel(x, y, matrix[y][x]);
+
         }
     }
-    Matrix w;
-    w.show();
+
+
+
+    int newWidth = cols * 20;
+       int newHeight = rows * 20;
+
+       QImage scaledImage = image.scaled(newWidth, newHeight, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    QLabel label;
+    label.setPixmap(QPixmap::fromImage(scaledImage));
+    label.show();
+
+
+//    QTranslator translator;
+//    const QStringList uiLanguages = QLocale::system().uiLanguages();
+//    for (const QString &locale : uiLanguages) {
+//        const QString baseName = "DSA_Project_" + QLocale(locale).name();
+//        if (translator.load(":/i18n/" + baseName)) {
+//            a.installTranslator(&translator);
+//            break;
+//        }
+//    }
+//    Matrix w;
+//    w.show();
     return a.exec();
 }
 
